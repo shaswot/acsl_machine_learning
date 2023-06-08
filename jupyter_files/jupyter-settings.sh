28a49f4b5311085e6d8d4474af944b6ccbc1abe0
Shaswot Shresthamali <shaswot@titanium.acsl.ics.keio.ac.jp>
2023-06-08 16:05:58 +0900

    configured overrides.json script

--- jupyter_files/jupyter-settings.sh
+++ jupyter_files/jupyter-settings.sh
@@ -2,4 +2,4 @@
 
 jupyter lab path | sed -n 's/Application directory:\s*\(.*\)/mkdir -p \1\/settings/p' | sh
 
-jupyter lab path | sed -n 's/Application directory:\s*\(.*\)/cp overrides.json \1\/settings/p' | sh
+jupyter lab path | sed -n 's/Application directory:\s*\(.*\)/cp /tmp/overrides.json \1\/settings/p' | sh
